785c69eb06a58b2e5cf99f8e0bd9e421146f0dde
Jan Wojtasinski <jan@829llc.com>
2016-06-29 10:19:08 -0400

    Add a set_api_key method to provide an override for the API key via WP filter.

--- inc/class-request.php
+++ inc/class-request.php
@@ -7,7 +7,7 @@ class Wunderground_Request {
 	 * @var string
 	 */
 	private $apiKey = '3ffab52910ec1a0e';
-
+        
 	/**
 	 * What features should be fetched by the request?
 	 *
@@ -72,12 +72,14 @@ class Wunderground_Request {
 	 * @param string  $units    [description]
 	 * @param boolean $cache    [description]
 	 */
-	function __construct( $location = '', $features = array(), $language = 'EN', $units = 'english', $cache = true ) {
-
+	function __construct( $location = '', $apiKey = '3ffab52910ec1a0e', $features = array(), $language = 'EN', $units = 'english', $cache = true ) {
+		
+		$this->set_api_key( $apiKey );
+		
 		$this->location = $location;
 
 		$features = wp_parse_args( $features, $this->features );
-
+        
 		$this->set_features( $features );
 		$this->set_language( $language );
 		$this->set_units( $units );
@@ -85,7 +87,16 @@ class Wunderground_Request {
 		$this->set_results();
 
 	}
-
+     
+    /**
+	 * Set the Wunderground API Key. This defaults to the plugins key, as set in the constructor.
+	 * Can be filtered using the `wunderground_request_atts` filter.
+	 * @param  string $apiKey The Wunderground API Key
+	 */  
+	private function set_api_key( $apiKey ) {
+		$this->apiKey = apply_filters( 'wunderground_api_key', $apiKey );
+	}
+        
 	private function set_cache( $cache ) {
 
 		if( isset($_GET['cache']) && current_user_can( 'manage_options' ) ) {
